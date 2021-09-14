9e56e258ffd1094e5cbb061f9904c74e48790d1c
Pavel <wol1414@gmail.com>
2021-09-14 15:20:30 +0300

    add video

--- public/js/common.js
+++ public/js/common.js
@@ -1,47 +1,43 @@
 "use strict";
-
 function setFixedNav() {
 	let topNav = document.querySelector('.top-nav  ');
 	if (!topNav) return;
-	window.scrollY > 0 ? topNav.classList.add('fixed') : topNav.classList.remove('fixed');
+	window.scrollY > 0
+		? topNav.classList.add('fixed')
+		: topNav.classList.remove('fixed');
 }
-
 function eventHandler() {
+
 	function whenResize() {
 		setFixedNav();
 	}
 
 	window.addEventListener('scroll', () => {
 		setFixedNav();
-	}, {
-		passive: true
-	});
+
+	}, { passive: true })
 	window.addEventListener('resize', () => {
 		whenResize();
-	}, {
-		passive: true
-	});
+	}, { passive: true });
+
 	whenResize();
+
+
 	let videoBlockPlay = document.querySelectorAll(".video-item__video");
 	videoBlockPlay.forEach(el => {
 		el.addEventListener('click', () => {
 			el.classList.add("play-video");
 			el.innerHTML = el.dataset.video;
-		});
-	});
-
-
-
-	
-}
-
-;
-
+		})
+	})
+};
 if (document.readyState !== 'loading') {
 	eventHandler();
 } else {
 	document.addEventListener('DOMContentLoaded', eventHandler);
-} // window.onload = function () {
+}
+
+// window.onload = function () {
 // 	document.body.classList.add('loaded_hiding');
 // 	window.setTimeout(function () {
 // 		document.body.classList.add('loaded');
